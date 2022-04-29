c761eaabaf1ee273b0b5e3121b43efa211d6bf20
ReddikHaien <584969@stud.hvl.no>
2022-04-29 17:57:52 +0200

    added code

--- ACCloudService/src/main/java/no/hvl/dat110/ac/restservice/AccessLog.java
+++ ACCloudService/src/main/java/no/hvl/dat110/ac/restservice/AccessLog.java
@@ -1,7 +1,9 @@
 package no.hvl.dat110.ac.restservice;
 
+import java.util.List;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.atomic.AtomicInteger;
+import java.util.stream.Collectors;
 
 import com.google.gson.Gson;
 
@@ -19,21 +21,21 @@ public class AccessLog {
 	// TODO: add an access entry to the log for the provided message and return assigned id
 	public int add(String message) {
 		
-		int id = 0;
+		int id = cid.addAndGet(1);
+		
+		this.log.put(id, new AccessEntry(id, message));
 		
 		return id;
 	}
 		
 	// TODO: retrieve a specific access entry from the log
 	public AccessEntry get(int id) {
-		
-		return null;
-		
+		return this.log.get(id);
 	}
 	
 	// TODO: clear the access entry log
 	public void clear() {
-		
+		this.log.clear();
 	}
 	
 	// TODO: return JSON representation of the access log
@@ -41,6 +43,10 @@ public class AccessLog {
     	
 		String json = null;
     	
-    	return json;
+		List<AccessEntry> entries = log.values().stream().collect(Collectors.toList());
+		
+		Gson gson = new Gson();
+		
+    	return gson.toJson(entries);
     }
 }
--- ACCloudService/src/main/java/no/hvl/dat110/ac/restservice/App.java
+++ ACCloudService/src/main/java/no/hvl/dat110/ac/restservice/App.java
@@ -4,6 +4,10 @@ import static spark.Spark.after;
 import static spark.Spark.get;
 import static spark.Spark.port;
 import static spark.Spark.put;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
 import static spark.Spark.post;
 import static spark.Spark.delete;
 
@@ -41,6 +45,60 @@ public class App {
 		 	Gson gson = new Gson();
 		 	
 		 	return gson.toJson("IoT Access Control Device");
+		});
+		
+		post("/accessdevice/log", (req, res) -> {
+			Gson gson = new Gson();
+			
+			AccessMessage message = gson.fromJson(req.body(), AccessMessage.class);
+			
+			int id = accesslog.add(message.getMessage());
+			
+			AccessEntry addedMessage = accesslog.get(id);
+			
+			return gson.toJson(addedMessage);
+		});
+		
+		get("/accessdevice/log",(req, res) -> {
+			
+			
+			return accesslog.toJson();
+		});
+		
+		get("/accessdevice/log/:id",(req, res) -> {
+			int p = 0;
+			try {
+				p = Integer.parseInt(req.params(":id"));
+			}catch(Exception e) {
+				res.status(400);
+				return "{ \"error\":\"Expected integer id\"}";
+			}
+			AccessEntry entry = accesslog.get(p);
+			if (entry == null) {
+				res.status(400);
+				return "{ \"error\":\"Invalid Id '" + p + "'\"}";
+			}
+			
+			Gson gson = new Gson();
+			
+			return gson.toJson(entry);
+		});
+		
+		put("/accessdevice/code", (req, res) -> {
+			Gson gson = new Gson();
+			AccessCode code = gson.fromJson(req.body(), AccessCode.class);
+			accesscode.setAccesscode(code.getAccesscode());
+			return gson.toJson(accesscode);
+		});
+		
+		get("/accessdevice/code", (req, res) -> {
+			Gson gson = new Gson();
+			return gson.toJson(accesscode);
+		});
+		
+		delete("accessdevice/log", (req, res) -> {
+			accesslog.clear();
+			return accesslog.toJson();
 		});
 		
 		// TODO: implement the routes required for the access control service
